a0c66111b320e38ab6a5a5a5b3f46457d8495a7d
DamianM <damian.michalski@kramp.com>
2019-03-07 13:44:24 +0100

    Poprawa Add comments

--- src/AddComment.js
+++ src/AddComment.js
@@ -1,9 +1,43 @@
 import React from 'react';
 
-const AddComment = ({text, addComment}) =>
-  <form>
-    <input className="text">{text}</input> 
-    <button onClick={() => addComment(document.querySelector('.text').value)} type='button'>Add comment</button> 
-  </form>;
+// const AddComment = ({text, addComment}) =>
+//
+//   <form>
+//     <input className="text">{text}</input>
+//     {/*<button onClick={() => addComment(document.querySelector('.text').value)} type='button'>Add comment</button>*/}
+//     <button onClick={() => addComment(this.text)} type='button'>Add comment</button>
+//   </form>;
+//
+// export default AddComment;
 
+
+class AddComment extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: ""
+    };
+  }
+  onSubmitHandler(event) {
+    const passInput = this.refs.pass;
+    if (passInput.value.length < 5) {
+      event.preventDefault();
+      this.setState({ message: "Password should be at least 5 chars long." });
+    } else {
+      this.setState({ message: "Comment added! " + passInput.value });
+    }
+  }
+  render() {
+    return (
+        <form >
+          <label>
+            Comment: <input type="text" ref="pass" />
+          </label>
+          <button onClick={this.onSubmitHandler.bind(this)} type='button'>Add comment</button>
+          <p>{this.state.message}</p>
+        </form>
+    );
+  }
+}
 export default AddComment;
+//ReactDOM.render(<PasswordForm />, document.getElementById("app"));
